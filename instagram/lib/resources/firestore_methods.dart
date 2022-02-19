045933bb763270ca383e2a65f47895f6c28087d1
bawar Khalid <bawarkhalid912@gmail.com>
2022-02-19 09:47:44 +0300

    delete post

--- instagram/lib/resources/firestore_methods.dart
+++ instagram/lib/resources/firestore_methods.dart
@@ -71,4 +71,11 @@ class FirestoreMethods{
       );
     }
   }
+  Future<void> deletePost(String postId) async {
+    try{
+    await  _firestore.collection('posts').doc(postId).delete();
+    }catch(err){
+      print(err.toString());
+    }
+  }
 }
