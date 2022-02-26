ac19b34c9f33780bb81e0654bc6e2c8f29de50c5
bawar Khalid <bawarkhalid912@gmail.com>
2022-02-26 18:24:25 +0300

    commit

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -6,11 +6,9 @@ import 'package:instagram/Widgets/post_card.dart';
 import 'package:instagram/provider/user_provider.dart';
 import 'package:instagram/responsive/mobile_screen_layout.dart';
 import 'package:instagram/responsive/resposnive_layout_screen.dart';
-
 import 'package:instagram/screens/login_Screen.dart';
 import 'package:instagram/screens/signup_screen.dart';
 import 'package:instagram/utills/colors.dart';
-
 import 'package:provider/provider.dart';
 
 void main() async{
@@ -58,9 +56,10 @@ class MyApp extends StatelessWidget {
               ),
             );
           }
-          return const LoginScreen();
+          return const SignupScreen();
         },
       ),
+
     ),
   );
   }
